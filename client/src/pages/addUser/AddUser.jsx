44ae737b7bc328598761a6f7dbc6202673145737
gabrieltrigueiro <gabrielcursos@proton.me>
2024-02-06 16:53:53 -0300

    validade email and phone

--- client/src/pages/addUser/AddUser.jsx
+++ client/src/pages/addUser/AddUser.jsx
@@ -3,16 +3,32 @@ import "./adduser.css";
 import axios from "axios"; 
 import { useNavigate } from "react-router-dom";
 
+
+
 export default function AddUser() {
 
-
+    const navigate = useNavigate(); 
+    const [validateError, setValidateError] = useState({
+        email: false, 
+        phone: false
+    });
     const [client, setClient] = useState({
         nome: '',
         email: '', 
         phone: '' 
     });
 
-    const navigate = useNavigate(); 
+    function isAPhone(pnumber) {
+        const patternForPhones = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/; 
+
+        return patternForPhones.test(pnumber); 
+    };
+
+    function isAMail(email) {
+        const patterForMail = /\S+@\S+\.\S+/; 
+
+        return patterForMail.test(email.toLowerCase()); 
+    };
 
     function handleClient(e) {
         const propertyChange = e.target.name; 
@@ -21,10 +37,20 @@ export default function AddUser() {
             ...client, 
             [propertyChange]: e.target.value
         });
+
+        setValidateError({
+            email: !isAMail(client.email),
+            phone: !isAPhone(client.phone)
+        });
     };
 
     function handleSubmit(e) {
         e.preventDefault(); 
+        
+        if (validateError.email || validateError.phone) {
+            return -1;
+        }
+
         axios.post("http://localhost:9000/adicionar", client).then((response) => console.log(response.data));
         navigate("/"); 
     };
@@ -32,6 +58,10 @@ export default function AddUser() {
     return (
         <div className="adduser">
             <h3 className="formTitle">Cadastro de Novo Cliente</h3>
+            {validateError.phone &&  <div class="alert"><i className="fa-solid fa-triangle-exclamation"></i> Informe um número de telefone válido.</div>      
+            }
+            {validateError.email &&  <div class="alert"><i className="fa-solid fa-triangle-exclamation"></i> Informe um email válido.</div>      
+            }
             <form className="userForm">
                 <div className="userFormGroup">
                     <label htmlFor="nameInput">Nome do Cliente</label>
